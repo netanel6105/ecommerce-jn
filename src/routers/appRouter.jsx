1e06f8571b5178dd4349d805a897d70f746c7ff1
netanel <netaneal6105@gmail.com>
2023-04-24 11:51:41 +0300

    ok

--- src/routers/appRouter.jsx
+++ src/routers/appRouter.jsx
@@ -2,10 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LayoutAdmin from "../layout/layoutAdmin/LayoutAdmin";
 import { ToastContainer } from "react-toastify";
 import LayoutUser from "../layout/layoutUser/LayoutUser";
+import { myContext } from "../context/myContext";
 
 const AppRouter = () => {
+
+
+
+  
   return (
     <Router>
+      <myContext.Provider value={{
+
+      }}>
       <Routes>
      
         <Route path="/" element={<LayoutUser/>} >
@@ -24,6 +32,7 @@ const AppRouter = () => {
       </Routes>
 
       <ToastContainer theme="colored" position="top-left" />
+      </myContext.Provider>
     </Router>
   );
 };
