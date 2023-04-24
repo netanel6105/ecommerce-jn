c66ac4e4a555126a4cf66d4974468a3ab0466c49
netanel <netaneal6105@gmail.com>
2023-04-24 14:04:58 +0300

    ok

--- src/layout/layoutAdmin/headerAdmin/HeaderAdmin.jsx
+++ src/layout/layoutAdmin/headerAdmin/HeaderAdmin.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-const HeaderAdmin = () => {
-
+function HeaderAdmin() {
   return (
     <div className="flex mt-2 items-center">
-    <div>
-      <Link to="/admin" className="ml-5 text-5xl">  Admin </Link>
-    </div>
+      <div>
+        <Link to="/admin" className="ml-5 text-5xl">
+          Admin
+        </Link>
+      </div>
 
-    <div>
-      <ul className="flex ml-[100px] text-4xl">
-        <li className="ml-9">Products</li>
-        <li className="ml-9">Categories</li>
-        <li className="ml-9">Users</li>
-      </ul>
+      <div>
+        <ul className="flex ml-[100px] text-4xl">
+          <li className="ml-9">Products</li>
+          <li className="ml-9">Categories</li>
+          <li className="ml-9">Users</li>
+          {/* <AccountCircleIcon /> */}
+        </ul>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default HeaderAdmin
+export default HeaderAdmin;
