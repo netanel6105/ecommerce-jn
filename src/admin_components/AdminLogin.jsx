96b2bb83c011ae0be9877615101d1c0ecf0c456c
netanel <netaneal6105@gmail.com>
2023-05-01 14:32:03 +0300

    ok

--- src/admin_components/AdminLogin.jsx
+++ src/admin_components/AdminLogin.jsx
@@ -6,64 +6,65 @@ import "./login.css";
 
 const AdminLogin = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    // let emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    const nav = useNavigate();
-  
-  
-    const onSub = (bodyData) => {
-      console.log(bodyData);
-      doApi(bodyData)
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  // let emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  const nav = useNavigate();
+
+
+  const onSub = (bodyData) => {
+    console.log(bodyData);
+    doApi(bodyData)
+  }
+
+  const doApi = async (bodyData) => {
+    try {
+      let url = API_URL + "/users/login";
+      let data = await doApiMethod(url, 'POST', bodyData)
+      console.log(data);
+      localStorage.setItem(TOKEN_KEY, data.token)
+      nav("/admin/users")
     }
-  
-    const doApi = async (bodyData) => {
-      try {
-        let url = API_URL + "/users/login";
-        let data = await doApiMethod(url, 'POST', bodyData)
-        console.log(data);
-        localStorage.setItem(TOKEN_KEY, data.token)
-        nav("/admin/users")
-      }
-      catch (err) {
-        console.log(err);
-        alert('email or pass wrong')
-      }
+    catch (err) {
+      console.log(err);
+      alert('email or pass wrong')
     }
+  }
 
 
   return (
-<div class="w-auto max-w-lg">
-  <form onSubmit={handleSubmit(onSub)} className="pl-[300px] px-8 pt-[100px] pb-8 mb-4 ">
+    <div className="container mx-auto">
 
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        email:
-      </label>
+      <form onSubmit={handleSubmit(onSub)} className="pl-[200px] px-8 pt-[100px] pb-8 mb-4 ">
 
-      <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })} 
-      className="shadow appearance-none border rounded w-96  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  type="text" placeholder="email"/>
-      {errors.email && <div className='text-red-600 '>*enter valid email</div>}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            email:
+          </label>
+
+          <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })}
+            className="shadow appearance-none border rounded w-96  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="email" />
+          {errors.email && <div className='text-red-600 '>*enter valid email</div>}
+        </div>
+
+
+        <div class="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" >
+            Password:
+          </label>
+          <input  {...register("password", { required: true, minLength: 3 })}
+            className="shadow appearance-none border rounded w-96 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="**********" />
+          {errors.password && <div className='text-red-600'>*enter valid password (min 3 chars)</div>}
+
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Login
+          </button>
+
+        </div>
+      </form>
     </div>
-
-
-    <div class="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2" >
-        Password:
-      </label>
-      <input  {...register("password", { required: true, minLength: 3 })} 
-      className="shadow appearance-none border rounded w-96 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"  type="password" placeholder="**********"/>
-       {errors.password && <div className='text-red-600'>*enter valid password (min 3 chars)</div>}
-      
-    </div>
-
-    <div className="flex items-center justify-between">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        Login
-      </button>
-     
-    </div>
-  </form>
-  </div>
 
   )
 }
