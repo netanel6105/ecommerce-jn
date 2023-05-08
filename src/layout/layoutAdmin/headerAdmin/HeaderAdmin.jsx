f8a646d1dea952893beb335545e2c22bca4d26fb
netanel <netaneal6105@gmail.com>
2023-05-08 14:03:41 +0300

    o

--- src/layout/layoutAdmin/headerAdmin/HeaderAdmin.jsx
+++ src/layout/layoutAdmin/headerAdmin/HeaderAdmin.jsx
@@ -1,32 +1,74 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import {FaBars, FaCommentAlt, FaShoppingBag, FaTh, FaUsers,FaSearchLocation,FaLockOpen, FaList} from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
-// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-function HeaderAdmin() {
+function HeaderAdmin({children}) {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen (!isOpen);
+  const menuItem = [
+    {
+      path:"/admin/dashboard",
+      name:"Dashboard",
+      icon:<FaTh/>
+  },
+    {
+      path:"/admin/login",
+      name:"Login",
+      icon:<FaLockOpen/>
+  },
+    {
+      path:"/admin/category",
+      name:"Category",
+      icon:<FaSearchLocation/>
+  },
+    {
+      path:"/admin/product",
+      name:"Product",
+      icon:<FaShoppingBag/>
+  },
+    {
+      path:"/admin/users",
+      name:"Users",
+      icon:<FaUsers/>
+  },
+    {
+      path:"/admin/comment",
+      name:"Comment",
+      icon:<FaCommentAlt/>
+  },
+    {
+      path:"/admin/todo",
+      name:"Todo",
+      icon:<FaList/>
+  },
+  ]
+    
+
   return (
-    <div className="flex mt-2 items-center">
-    <div>
-      <Link to="/admin" className="ml-[200px] text-5xl">
-        Admin
-      </Link>
+  
+    <div className="flex">
+    <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar ">
+        <div className="top_section">
+            <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Admin</h1>
+            <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                <FaBars onClick={toggle}/>
+            </div>
+        </div>
+        {
+            menuItem.map((item, index)=>(
+                <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                    <div className="icon">{item.icon}</div>
+                    <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                </NavLink>
+            ))
+        }
     </div>
+    
+    <main>{children}</main>
 
-    <div>
-      <div className="flex ml-[100px] text-4xl">
-        <Link className="ml-9">Products</Link>
-        <Link className="ml-9">Categories</Link>
-        <Link to="/admin/users" className="ml-9">
-          Users
-        </Link>
-        {/* <AccountCircleIcon /> */}
-      </div>
-    </div>
-
-    <Link className="ml-[150px] text-4xl" to="/admin/login">
-      Login
-    </Link>
-  </div>
+   
+ </div>
   );
 }
 
