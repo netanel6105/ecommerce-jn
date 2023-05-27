3c440ef13a63e727e69b89a87ec620883fd12eaf
netanel <netaneal6105@gmail.com>
2023-05-27 23:44:45 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import { MyContext } from "./context/myContext";
 import AppRouter from "./routers/appRouter";
+import { TOKEN_KEY } from "./services/services";
 
 
 function App() {
+  const [user , setUser] = useState(null);
+
+  const logout = async() =>{
+    localStorage.removeItem(TOKEN_KEY);
+    // setUser(null);
+  }
   
   return (
     <div className="App">
       <MyContext.Provider value={{
-
+        logout
       }}>
 
       <AppRouter />
