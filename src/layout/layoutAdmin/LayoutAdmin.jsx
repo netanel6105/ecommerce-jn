c37f39ab1dc226a0386c1d1bb7a4decfba73162d
netanel <netaneal6105@gmail.com>
2023-05-28 12:27:01 +0300

    ok

--- src/layout/layoutAdmin/LayoutAdmin.jsx
+++ src/layout/layoutAdmin/LayoutAdmin.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeaderAdmin from './headerAdmin/HeaderAdmin'
 import {Outlet} from 'react-router-dom'
+import Navbar from './headerAdmin/Navbar'
 const LayoutAdmin = () => {
   return (
-    <div className='flex'>
+    <div className=''>
         <HeaderAdmin/>
         <Outlet/>
     </div>
