ab10321d40318ab567a34a329d0d59298903ff69
netanel <netaneal6105@gmail.com>
2023-05-31 20:27:13 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import { useState } from "react";
+
 import "./App.css";
-import { MyContext } from "./context/myContext";
-import AppRouter from "./routers/appRouter";
+
+
 import { TOKEN_KEY } from "./services/services";
+
+import React, { useState } from 'react';
+import {MyContext} from './context/myContext'; // Import MyContext from the appropriate file
+import AppRouter from './routers/appRouter'; // Import AppRouter from the appropriate file
+
+
+// const [addToDo, setToDo] = useState([
+//   "Add another component to Tailwind Components",
+//   "Submit Todo App Component to Tailwind Components"
+// ])
+
+// function App() {
+  
+//   const [product_ar, setProduct_ar] = useState(["milk","bamba","egge"]);
+  
+//   const addNewToDo = (mission) =>{
+//     setProduct_ar([...product_ar,mission])
+//   }
+
+//   const logout = async() =>{
+//     localStorage.removeItem(TOKEN_KEY);
+   
+//   }
+  
+//   return (
+//     <div className="App">
+//       <MyContext.Provider value={{
+//         logout,
+//         product_ar,addNewToDo
+//       }}>
+
+//       <AppRouter />
+//       </MyContext.Provider>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 function App() {
-  const [user , setUser] = useState(null);
+  const [product_ar, setProduct_ar] = useState(["milk","bamba","egge"]);
 
-  const logout = async() =>{
+  const addNewToDo = (mission) => {
+    setProduct_ar([...product_ar, mission]);
+  };
+
+  const logout = async () => {
     localStorage.removeItem(TOKEN_KEY);
-    // setUser(null);
-  }
-  
+  };
+
   return (
     <div className="App">
-      <MyContext.Provider value={{
-        logout
-      }}>
-
-      <AppRouter />
+      <MyContext.Provider
+        value={{
+          logout,
+          product_ar,
+          addNewToDo
+        }}
+      >
+        <AppRouter />
       </MyContext.Provider>
     </div>
   );
--- src/admin_components/pages/TodoList.jsx
+++ src/admin_components/pages/TodoList.jsx
@@ -1,37 +1,19 @@
-import React from "react";
+import React, { useContext } from 'react'
+import { MyContext } from '../../context/myContext'
 
 const ToDolist = () => {
+  const {product_ar}= useContext(MyContext)
   return (
-    <div className="w-screen flex  justify-center">
-
- {/* <!-- component --> */}
-
- <div class="bg-white rounded shadow  p-6 m-4 w-[900px]">
-        <div class="mb-4">
-            <h1 class="text-grey-darkest">Todo List</h1>
-            <div class="flex mt-4">
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo"/>
-                <button class="flex-no-shrink p-2 border-2 rounded text-teal-500 border-teal-500 hover:text-white hover:bg-teal-500">Add</button>
-            </div>
-        </div>
-        <div>
-            <div class="flex mb-4 items-center">
-                <p class="w-full text-grey-darkest">Add another component to Tailwind Components</p>
-                <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
-                <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-500">Remove</button>
-            </div>
-          	<div class="flex mb-4 items-center">
-                <p class="w-full line-through text-green">Submit Todo App Component to Tailwind Components</p>
-                <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">Not Done</button>
-                <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-500">Remove</button>
-            </div>
-        </div>
-    </div>
-    </div>
+    <div>
+      
+      <h2>ToDolist</h2>
+      {product_ar.map((itemVal,i)=>{
+        return(
+          <h3 key={i}>{itemVal}</h3>
+        )
+      })}
+      </div>
   )
-   
+}
 
- 
-};
-
-export default ToDolist;
+export default ToDolist
